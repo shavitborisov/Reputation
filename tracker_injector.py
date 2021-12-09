e461095a0067e323f3dcce8a41ef2ab452cbf96c
galls2 <galls2@campus.technion.ac.il>
2021-12-09 11:20:56 +0200

    Added support for mutiple photos, missing message sent with each photo

--- tracker_injector.py
+++ tracker_injector.py
@@ -2,16 +2,23 @@ from bot.bot import stream
 from image.image_util import creator
 import time
 
+def convert_pic_and_send(bot, message):
+	creator("bla.jpg", message).save("bla1.bmp")
+	bot.send_pic("bla1.bmp")
+
 def main():
 	bot = stream()
 
 	while True:
-		message = bot.read_pic_and_message("bla.jpg")
+		messages = bot.read_pic_and_message("bla.jpg")
 		time.sleep(0.4)
 
-		creator("bla.jpg", message).save("bla1.bmp")
+		for msg in messages.split("\n"):
+			convert_pic_and_send(bot, msg)
+			time.sleep(1)
 
-		bot.send_pic("bla1.bmp")
+		time.sleep(2)
+		
 		bot.stream_write("The name was successfully hidden")
 		bot.stream_write("Always at your service, Mr. TrackerInjector")
 
