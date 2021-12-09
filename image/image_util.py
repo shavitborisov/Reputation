46447a45297d8f4b9f28ec6255cb045abf4f925a
Matan Kotick <matank001@gmail.com>
2021-12-09 11:22:29 +0200

    Nice Refactor

--- image/image_util.py
+++ image/image_util.py
@@ -1,21 +1,20 @@
 from PIL import Image
 import collections
 import random
+from reedsolo import RSCodec
 
 imagePath = 'example.jpg'
 newImagePath = 'result.jpg'
 
 def return_8_bit(number):
-    binary_number = bin(number)
-    if len(binary_number) < 10:
-        return binary_number[0:2] + ('0' * (10 - len(binary_number))) + binary_number[2:]
-    return binary_number
+    bin_num = bin(number)
+    return bin_num[:2] + ('0' * (10 - len(bin_num))) + bin_num[2:]
 
-EXAMPLE_STRING = "My Name Is Kotickk"
-MAJOR = 5
+EXAMPLE_STRING = "Avia's Picture"
+MAJOR = 8
 BIT_LOCATION = 2
-DECODE_TIMES = 2
-SEED = 0xdeafbeaf
+DECODE_TIMES = 1
+SEED = 0xdeadbeaf
 
 def get_string_current_bit(string_to_code):
     string_to_code = chr(len(string_to_code)) + string_to_code
@@ -25,12 +24,11 @@ def get_string_current_bit(string_to_code):
                 for j in range(MAJOR):
                     yield bit
 
-def index_generator_one(image_length):
+def index_generator_one(offset, image_length):
     index_array = 0
     while index_array < image_length:
-        if index_array % 600 == 0:
-            yield index_array
-        index_array += 1
+        index_array += offset
+        yield index_array
 
 def index_generator_two(image_length):
     random.seed(SEED)
@@ -43,8 +41,14 @@ def index_generator_two(image_length):
         values.add(current_value)
         yield current_value
 
-def index_generator(image_length):
-    return index_generator_two(image_length)
+def index_generator_three(offset, width, height):
+    index_array = 0
+    for i in range(height):
+        for j in range(width):
+            yield 16
+
+def index_generator(width, height):
+    return index_generator_two(width * height)
 
 def write_on_msb(number, bit):
     binary_number = return_8_bit(number)
@@ -57,8 +61,11 @@ def creator (source, string_to_encode):
     im = Image.open(source)
     newimdata = [color for color in im.getdata()]
 
+    #rsc = RSCodec(100)  # 10 ecc symbols
+    #rs_string = rsc.
+
     message_generator = get_string_current_bit(string_to_encode)
-    index_gen = index_generator(len(im.getdata()))
+    index_gen = index_generator(*im.size)
     for current_bit in message_generator:
         index = next(index_gen)
         color = newimdata[index]
@@ -86,25 +93,18 @@ def investigator(source):
     current_string = ""
     current_character = ""
     
-    message = get_string_current_bit(EXAMPLE_STRING)
-    errors = 0
-    number = 0
     first_byte = True
     message_length = 0
     decoded_messages = 0
-    index_gen = index_generator(len(im.getdata()))
+    index_gen = index_generator(*im.size)
     for index in index_gen:
         color = im.getdata()[index]
-        number += 1
         
         current_bit_str = return_8_bit(color[0])[BIT_LOCATION] + \
                         return_8_bit(color[1])[BIT_LOCATION] + \
                         return_8_bit(color[2])[BIT_LOCATION]
         current_bit = collections.Counter(current_bit_str).most_common(1)[0][0]
         bit_collector += current_bit
-        
-        if current_bit != next(message):
-            errors += 1
 
         if len(bit_collector) == MAJOR:
             real_bit = collections.Counter(bit_collector).most_common(1)[0][0]
@@ -127,11 +127,9 @@ def investigator(source):
                             break
                 current_character = ""
             bit_collector = ""
-    
-    print (str(errors) + "/" + str(number))
-    print (str(float(errors) * (100.0 / number)))
 
-    return all_messages[0]
+    return most_common(all_messages)
 
-creator(imagePath, EXAMPLE_STRING).save(newImagePath)
-print (investigator(newImagePath))
+if __name__ == '__main__':
+    creator(imagePath, EXAMPLE_STRING).save(newImagePath)
+    print (investigator(newImagePath))
